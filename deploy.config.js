f226a28e135855768893ea0a948c3c222ace7cc9
Bryan Karta <65345089+Servond@users.noreply.github.com>
2023-04-18 16:02:07 +0700

    Update deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "./projects/server/src/index.js",
       env: {
         NODE_ENV: "production",
-        PORT: 8010601,
+        PORT: 8601,
       },
       time: true,
     },
