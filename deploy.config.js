c32c4fe256b3828ae058f709f24c57aa7873827c
Bryan Karta <65345089+Servond@users.noreply.github.com>
2023-04-18 16:55:29 +0700

    Update deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "JCWD-0106-01", // Format JCWD-{batchcode}-{groupnumber}
-      script: "./projects/server/src/index.js",
+      script: "./projects/server/src/index.cjs",
       env: {
         NODE_ENV: "production",
         PORT: 8601,
