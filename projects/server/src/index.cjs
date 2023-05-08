1fc764315ffc4f00c6e894c30526ca2a75b96c97
Bryan Karta <65345089+Servond@users.noreply.github.com>
2023-05-08 19:32:27 +0700

    Update index.cjs

--- projects/server/src/index.cjs
+++ projects/server/src/index.cjs
@@ -2,7 +2,7 @@
 
 const path = require("path");
 require("dotenv").config({
-  path: path.resolve(__dirname, "../.env");  
+  path: path.resolve(__dirname, "../.env")
 });
 const express = require("express");
 const cors = require("cors");
